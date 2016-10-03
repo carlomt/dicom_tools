cdc32a5a0626874261f79cd826c6fe5332902ef3
Carlo Mancini <carlo@accatagliato.org>
2016-10-03 15:43:18 +0200

    normalizzato alla media della fetta

--- dicom_tools/make_histo.py
+++ dicom_tools/make_histo.py
@@ -1,15 +1,16 @@
 import ROOT
 
 def make_histo(data, mask):
-    nbin = 101
+    nbin = 100
 
-    his = ROOT.TH1F("histo","histo",nbin,-0.5,2000.5)
+    his = ROOT.TH1F("histo","histo",nbin,0,3)
     for fetta,fettaROI in zip(data,mask) :
         if fettaROI.max() > 0 :
+            norm = fetta.mean()
             for riga, rigaROI in zip(fetta,fettaROI) :
                 for val, inROI in zip(riga, rigaROI) :
                     if inROI>0 :
-                        his.Fill(val)
+                        his.Fill(val/norm)
                         # print(val)
 
     return his

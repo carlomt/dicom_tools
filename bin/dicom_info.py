0ffd48ee0ab3336474d0b9eef98eebe5bc532cc0
Carlo Mancini <carlo@accatagliato.org>
2016-10-16 09:35:50 +0200

    first commit

--- bin/dicom_info.py
+++ bin/dicom_info.py
@@ -5,6 +5,65 @@ import argparse
 import dicom
 import ROOT
 
-outfname="out.root"
+outfname="out.csv"
 
 inpath="."
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+parser.add_argument("inputpath", help="path of the DICOM directory (default ./)")
+parser.add_argument("-o", "--outfile", help="define output file name (default out.csv)")
+
+args = parser.parse_args()
+
+if args.inputpath:
+    inpath = args.inputpath
+
+if args.outfile:
+    outfname = args.outfile
+
+dirs = glob.glob(inpath+"/*/")
+
+if args.verbose:
+    print "input directory:\n",inpath
+    print len(dirs)," directories"
+
+table = []
+header = []
+    
+for thisdir in dirs:
+    os.chdir(inpath)
+    infiles=glob.glob("*.dcm")
+    
+    for filen,thisfile in enumerate(infiles):
+        thisdicom = dicom.read_file(thisfile)
+        line = []
+        line.append(thisdir)
+        
+        if filen <=1 :
+            for attr, value in thisdicom.__dict__.iteritems():
+                header.append(value)
+
+        out_file.write(filen+", "+thisfile+", ")
+        for attr, value in thisdicom.__dict__.iteritems():
+            line.append(value)
+            out_file.write(value+", ")
+        out_file.write("\n")
+
+    if line not in table:
+        talbe.append(line)
+
+
+out_file = open(outfname,"w")
+for element in header:
+    out_file.write(element+", ")
+out_file.write("\n")
+
+for line in table:
+    for element in line:
+        out_file.write(element+", ")
+    out_file.write("\n")
+
+        
+out_file.close()

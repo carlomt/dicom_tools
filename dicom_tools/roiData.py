1fabf27c6092cfa1950f72682ad1f8bc8f1d3b51
Carlo Mancini <carlo@accatagliato.org>
2016-12-15 00:01:27 +0100

    update on reading and writing on roi files

--- dicom_tools/roiData.py
+++ dicom_tools/roiData.py
@@ -1,11 +1,12 @@
 class roiData:
     
-    def __init__(self, roistates, dicomsPath=None):
+    def __init__(self, roistates, numberOfROIs, dicomsPath=None):
         self.layers = []
         self.roistates = []
         self.dicomsPath=dicomsPath
         self.originalLenght = len(roistates)
-
+        self.numberOfROIs = numberOfROIs
+        
         for i, roistate in enumerate(roistates):
             if roistate:
                 self.layers.append(i)

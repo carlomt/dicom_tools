2ba49ee8dc2b853ecf2540efa4ddb88736a42913
Carlo Mancini <carlo@accatagliato.org>
2016-12-20 15:01:41 +0100

    first commit

---
+++ dicom_tools/roi2myroi.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.spatial import ConvexHull
+import dicom_tools.pyqtgraph as pg
+
+def roi2myroi(ROI, verbose=False):
+    rois = [None]*len(ROI)
+    firsttime = True
+    
+    for layer in xrange(0, len(ROI)):
+        fetta = ROI[layer]
+        apoints = []
+        if fetta.max() > 0 :
+            for j, riga in enumerate(fetta):
+                for i, elemento in enumerate(riga):
+                    if elemento:
+                        thispoint = [i,j]
+                        apoints.append(thispoint)
+            points = np.array(apoints)
+            if firsttime:
+                firsttime = False
+                for point in points:
+                    print point
+                        
+            hull = ConvexHull(points)
+            rois[layer] = pg.PolyLineROI(hull.simplices.tolist(), pen=(6,9), closed=True).saveState()
+    return rois
+            # print "hull.simplices", hull.simplices

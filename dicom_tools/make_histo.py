resolved bug of normalization from myroi files

dicom_tools/make_histo.py
@@ -2,7 +2,7 @@ import ROOT
 import numpy as np
 # from tabulate import tabulate
 
-def make_histo(data, mask, suffix="", verbose=False, ROInorm=False):
+def make_histo(data, mask, suffix="", verbose=False, ROInorm=False, normalize=False):
     nbin = 200
     binmin=data.min() *0.8
     binmax=data.max() *1.2
@@ -30,16 +30,22 @@ def make_histo(data, mask, suffix="", verbose=False, ROInorm=False):
         fettaROI = mask[layer]
         # res = []
         thishisto = ROOT.TH1F("h"+str(layer)+suffix,"h"+str(layer),nbin,binmin,binmax)
+        meaninroi = 0
         if fettaROI.max() > 0 :
             # res.append(layer)
             for val, inROI in zip(np.nditer(fetta),np.nditer(fettaROI)):
                 if inROI>0 :
-                    if ROInorm.any():
-                        normarea = ROInorm[layer]*data[layer]
-                        meaninroi = normarea.mean()
-                        val = val/meaninroi
+                    if normalize:
+                        if ROInorm.any():
+                            normarea = ROInorm[layer]*data[layer]
+                            meaninroi = normarea.mean()
+                            val = val/meaninroi
                     his.Fill(val)
                     thishisto.Fill(val)
+
+            if verbose:
+                print("make_histo: layer",layer,"meaninroi",meaninroi)
+                    
             hEntries.SetBinContent(layer,thishisto.GetEntries())
             hMean.SetBinContent(layer,thishisto.GetMean())
             hStdDev.SetBinContent(layer,thishisto.GetStdDev())

dicom_tools/myroi2roi.py
@@ -5,19 +5,19 @@ from dicom_tools.roiFileHandler import roiFileHandler
 
 def myroi2roi(myrois, shape, verbose=False):
     if verbose:
-        print("myroi2roi: called \n")
+        print("myroi2roi: called")
     outroi = np.full(shape,False,dtype=bool)
     if len(myrois) != len(outroi):
         print("error: len rois = ",len(rois)," but len dicom=",len(outroi))
 
-    for n, myroi, layer in enumerate(zip(myrois,outroi)):
+    for n, myroi in enumerate(myrois):
         if not myroi is None:
-            layer = grid_points_in_poly(layer.shape, myroi['points'])
+            outroi[n] = grid_points_in_poly(outroi[n].shape, myroi['points'])
             if verbose:
-                print("myroi2roi: layer",n,"tot true pixels",layer.sum())
+                print("myroi2roi: layer",n,"tot true pixels",outroi[n].sum())
         elif verbose:
             print("myroi2roi: layer",n,"myroi is None")
-                
+        
     if verbose:
         print("myroi2roi: returning \n")
     return outroi
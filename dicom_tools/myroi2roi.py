833ef5eeb50e0e86cdc75f99e64be4765d347eaa
Carlo Mancini <carlo@accatagliato.org>
2017-01-25 22:57:26 +0100

    handle also one layer

--- dicom_tools/myroi2roi.py
+++ dicom_tools/myroi2roi.py
@@ -7,16 +7,20 @@ def myroi2roi(myrois, shape, verbose=False):
     if verbose:
         print("myroi2roi: called")
     outroi = np.full(shape,False,dtype=bool)
-    if len(myrois) != len(outroi):
-        print("error: len rois = ",len(rois)," but len dicom=",len(outroi))
+  
 
-    for n, myroi in enumerate(myrois):
-        if not myroi is None:
-            outroi[n] = grid_points_in_poly(outroi[n].shape, myroi['points'])
-            if verbose:
-                print("myroi2roi: layer",n,"tot true pixels",outroi[n].sum())
-        elif verbose:
-            print("myroi2roi: layer",n,"myroi is None")
+    if outroi.ndim > 2:
+        if len(myrois) != len(outroi):
+            print("error: len rois = ",len(myrois)," but len dicom=",len(outroi))
+        for n, myroi in enumerate(myrois):
+            if not myroi is None:
+                outroi[n] = grid_points_in_poly(outroi[n].shape, myroi['points'])
+                if verbose:
+                    print("myroi2roi: layer",n,"tot true pixels",outroi[n].sum())
+            elif verbose:
+                print("myroi2roi: layer",n,"myroi is None")
+    else:
+        outroi = grid_points_in_poly(outroi.shape, myrois['points'])
         
     if verbose:
         print("myroi2roi: returning \n")

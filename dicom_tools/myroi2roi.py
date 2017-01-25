a4e410c1e13484b22209db6b2ee3da2a24f647f3
Carlo Mancini <carlo@accatagliato.org>
2017-01-25 11:03:08 +0100

    added more verbose out

--- dicom_tools/myroi2roi.py
+++ dicom_tools/myroi2roi.py
@@ -10,9 +10,14 @@ def myroi2roi(myrois, shape, verbose=False):
     if len(myrois) != len(outroi):
         print("error: len rois = ",len(rois)," but len dicom=",len(outroi))
 
-    for myroi, layer in zip(myrois,outroi):
+    for n, myroi, layer in enumerate(zip(myrois,outroi)):
         if not myroi is None:
             layer = grid_points_in_poly(layer.shape, myroi['points'])
+            if verbose:
+                print("myroi2roi: layer",n,"tot true pixels",layer.sum())
+        elif verbose:
+            print("myroi2roi: layer",n,"myroi is None")
+                
     if verbose:
         print("myroi2roi: returning \n")
     return outroi

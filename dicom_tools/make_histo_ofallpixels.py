076e6755582f743ac3dfe13a5e124274156e6b61
Carlo Mancini <carlo@accatagliato.org>
2017-02-04 11:44:16 +0100

    istogramma senza roi, di tutto

---
+++ dicom_tools/make_histo_ofallpixels.py
@@ -0,0 +1,22 @@
+import ROOT
+import numpy as np
+# from tabulate import tabulate
+
+def make_histo_ofallpixels(data, suffix="", verbose=False, ROInorm=False, normalize=False):
+    nbin = 200
+    binmin=data.min() *0.8
+    binmax=data.max() *1.2
+    nFette = len(data)
+
+    allhistos = []
+    
+    for layer in xrange(0,nFette):
+        fetta = data[layer]
+        
+        thishisto = ROOT.TH1F("h"+str(layer)+suffix,"h"+str(layer),nbin,binmin,binmax)
+
+        for val in np.nditer(fetta):
+            thishisto.Fill(val)
+        allhistos.append(thishisto)
+
+    return allhistos

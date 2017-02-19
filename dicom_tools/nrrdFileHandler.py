505f2a852d9d7149e541bcf29452d08fc5624f69
Carlo Mancini <carlo@accatagliato.org>
2017-02-19 16:07:43 +0100

    save roi to nrrd

--- dicom_tools/nrrdFileHandler.py
+++ dicom_tools/nrrdFileHandler.py
@@ -11,5 +11,17 @@ class nrrdFileHandler:
         nrrdROItmp, nrrdROIoptions = nrrd.read(inFileName)
         nrrdROI = nrrdROItmp.swapaxes(0,1).swapaxes(0,2)
         return nrrdROI[::-1,:,::-1]
+
+    def write(self, outFileName, data):
+        if self.verbose:
+        print("nrrdFileHandler: type(data)",type(data),"len(data)",len(data))
+            
+        outData = data.astype(int)
+        outData = outData[::-1,:,:]
+        outData = outData[:,:,::-1]
+        outData= outData.swapaxes(0,2).swapaxes(0,1)
+        if not str(outFileName).endswith('.nrrd'):
+            filename = outFileName+".nrrd"
+        nrrd.write(outFileName, outData)
     
     

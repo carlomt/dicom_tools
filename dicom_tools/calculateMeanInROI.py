e5ed48bd56fd82447f4c2d1ec5726f554913d724
Carlo Mancini <carlo.mancini.terracciano@roma1.infn.it>
2017-02-21 12:30:47 +0100

    cambiato modo di mostrare media della roi

--- dicom_tools/calculateMeanInROI.py
+++ dicom_tools/calculateMeanInROI.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def calculateMeanInROI(data, roi, verbose=False):
-    if verbose:
-        print("calculateMeanInROI: data.shape()",data.shape())
     normarea = roi*data
     meaninroi = normarea.sum()/np.count_nonzero(normarea)
-
+    if verbose:
+        print("calculateMeanInROI: data.shape",data.shape)
+        print("calculateMeanInROI: returning mean",meaninroi) 
     return meaninroi

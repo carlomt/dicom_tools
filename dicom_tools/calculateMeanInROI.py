e60139e1903cacc5b682aa556fcab16b6dc61112
Carlo Mancini <carlo@accatagliato.org>
2017-02-22 09:58:24 +0100

    risolto bug interfaccia grafica e ottimizzato calcolo media in roi

--- dicom_tools/calculateMeanInROI.py
+++ dicom_tools/calculateMeanInROI.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 def calculateMeanInROI(data, roi, verbose=False):
-    normarea = roi*data
-    meaninroi = normarea.sum()/np.count_nonzero(normarea)
     if verbose:
+        normarea = roi*data
+        meaninroi = normarea.sum()/np.count_nonzero(normarea)
+        
         print("calculateMeanInROI: data.shape",data.shape)
-        print("calculateMeanInROI: returning mean",meaninroi) 
-    return meaninroi
+        print("calculateMeanInROI: returning mean",meaninroi)
+        print("calculateMeanInROI: normarea.mean()",normarea.mean())
+        print("calculateMeanInROI: np.ma.average(data,weights=roi)",np.ma.average(data,weights=roi))        
+        mx = np.ma.masked_array(data,mask=np.logical_not(roi))
+        print("calculateMeanInROI: mx.mean()",mx.mean())
+    return np.ma.average(data,weights=roi)

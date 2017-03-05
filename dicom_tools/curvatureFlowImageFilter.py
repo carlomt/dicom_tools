624a7cd61f6ff31034d5abf6069f6af067e0e379
Carlo Mancini <carlo@accatagliato.org>
2017-03-05 20:30:25 +0100

    sync

---
+++ dicom_tools/curvatureFlowImageFilter.py
@@ -0,0 +1,20 @@
+import numpy as np
+import SimpleITK as sitk
+
+# https://itk.org/SimpleITKDoxygen/html/classitk_1_1simple_1_1CurvatureFlowImageFilter.html#details
+
+def curvatureFlowImageFilter(img, verbose=False):
+
+    imgOriginal = img
+    convertOutput = False
+    if type(img) != sitk.SimpleITK.Image:
+        imgOriginal = sitk.GetImageFromArray(img)
+        convertOutput = True
+        
+    imgSmooth = sitk.CurvatureFlow(image1=imgOriginal,
+                                        timeStep=0.125,
+                                        numberOfIterations=5)
+    if convertOutput:
+        imgSmooth = sitk.GetArrayFromImage(imgSmooth)
+
+    return imgSmooth

bc250103530da0e8dd1d3a8ba84d4f34038761ac
Carlo Mancini <carlo@accatagliato.org>
2018-04-22 15:19:35 +0200

    update

--- bin/dicom_analyze_db.py
+++ bin/dicom_analyze_db.py
@@ -4,9 +4,6 @@ import os
 import glob
 import argparse
 import numpy as np
-
-from dicom_tools.dicom import mydicom
-
 from dicom_tools.FileReader import FileReader
 from dicom_tools.info_file_parser import info_file_parser
 from dicom_tools.myroi2roi import myroi2roi

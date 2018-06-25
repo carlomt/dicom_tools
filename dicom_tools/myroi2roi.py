72a696ed04fb342afeb5d368c072d4034c475774
Carlo Mancini <carlo.mancini.terracciano@roma1.infn.it>
2018-06-25 13:12:27 +0200

    bug on roi conversion fixed

--- dicom_tools/myroi2roi.py
+++ dicom_tools/myroi2roi.py
@@ -7,7 +7,9 @@ from dicom_tools.roiFileHandler import roiFileHandler
 def convert1roi(myroi, layershape, verbose=False):
     points = np.array(myroi['points'])
     shift = np.array(myroi['pos'])
+    shift -= np.array([0.5,0.5])
     if verbose:
+        print(type(shift))
         print(myroi)
         print(points+shift)
     return grid_points_in_poly(layershape, points+shift)    
